678a09683fea4396af46223e5339a787106735ae
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-16 15:32:36 +0600

    added public route in separate components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Login from './pages/Login';
-import Services from './pages/Services';
 import { Route, Routes } from 'react-router-dom';
+import {publicRoute} from './routes/PublicRoute';
 
 function App() {
   return (
     <div className="">
       <Navbar>
         <Routes>
-          <Route path="/" element={<Home></Home>}></Route>
-          <Route path="/about" element={<About></About>}></Route>
-          <Route path="/contact" element={<Contact></Contact>}></Route>
-          <Route path="/login" element={<Login></Login>}></Route>
-          <Route path="/service" element={<Services></Services>}></Route>
+          {publicRoute.map(({ path, Component }, index) => (
+            <Route key={index} path={path} element={<Component />} />
+          ))}
         </Routes>
       </Navbar>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,27 @@ module.exports = {
           error: '#FA5C5C',
         },
       },
-      "dark"
+      {
+        dark: {
+          primary: '#D9F99D',
+
+          secondary: '#FDE68A',
+
+          accent: '#4B5563',
+
+          neutral: '#F3F4F6',
+
+          'base-100': '#1F2937',
+
+          info: '#98A8DD',
+
+          success: '#1BBB70',
+
+          warning: '#F59E0B',
+
+          error: '#FB7185',
+        },
+      },
     ],
   },
   plugins: [require('daisyui')],
