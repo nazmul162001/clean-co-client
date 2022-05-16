3e30e18707a0785f9d8566cb5f5939e22d0f80e6
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-16 20:22:13 +0600

    added login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,8 +2,41 @@ import React from 'react';
 
 const Login = () => {
   return (
-    <div>
-      <h2>this is my login page</h2>
+    <div class="hero h-[calc(100vh-64px)] bg-accent w-full">
+      <div class="card  w-1/3 shadow-2xl bg-base-100">
+        <div class="card-body">
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Email</span>
+            </label>
+            <input
+              type="text"
+              placeholder="email"
+              class="input input-bordered"
+            />
+          </div>
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Password</span>
+            </label>
+            <input
+              type="text"
+              placeholder="password"
+              class="input input-bordered"
+            />
+            <label class="label">
+              <a href="#" class="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div class="form-control mt-6">
+            <button class="btn btn-primary">Login</button>
+          </div>
+          <div class="divider">OR</div>
+          <button className='btn btn-glass hover:btn-primary'>Login with Google</button>
+        </div>
+      </div>
     </div>
   );
 };
