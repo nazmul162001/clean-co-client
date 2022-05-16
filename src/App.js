5194c399d23b158a12331ae76793ccb3203dbb3d
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-16 12:27:48 +0600

    customize tailwind theme & color

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <h2 className="text-3xl text-center text-red-700 font-bold">This is my clean-co website</h2>
+      <button class="btn btn-primary">Button</button>
     </div>
   );
 }
